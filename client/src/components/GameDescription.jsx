ef359c9241742e78d8cbb3e2aa31d3857e5c7279
alphie1028 <alphie1028@gmail.com>
2023-04-07 17:58:30 -0400

    alot of styling done

--- client/src/components/GameDescription.jsx
+++ client/src/components/GameDescription.jsx
@@ -1,38 +1,40 @@
 import React, {useState, useEffect} from "react";
 import pic from '/src/assets/game-description-image.png';
 import UserDefinedTags from "./UserDefinedTags";
+import "/src/styles/GameDescription.css"
 
 function GameDescription(){
     return(
-        <div>
-            <div>
-                <img src={pic}/>
+        <div className="gameDescription">
+            <div className="pictureDiv">
+                <img className="pic"src={pic}/>
             </div>
-            
-            <div>
+            <div className="description">
                 Named Game of the Year by over 50 publications, 
                 Valve's debut title blends action and adventure with award-winning technology to create a frighteningly realistic world where players must think to survive. 
                 Also includes an exciting multiplayer mode that allows you to play against friends and enemies
                 
             </div>
-            <div>
-                Recent Reviews: <span>Overwelmingly Possive 1,666</span>
+            <div className="reviewSum">
+                <div className="reviewText">Recent Reviews:</div> <span className="reviewSpan">Overwelmingly Possive</span>
             </div>
-            <div>
-                All Reviews: <span>Overwelmingly Positive 70,356</span>
+            <div className="reviewSum">
+                <div className="reviewText">All Reviews:</div> <span className="reviewSpan">Overwelmingly Positive</span>
             </div>
-            <div>
-                <div>Release Date:</div> <div>Nov 8, 1998</div>
+            <div className="releaseSum">
+                <div className="releaseText">Release Date: </div> <div className="releaseDate">Nov 8, 1998</div>
             </div>
-            <div>
-                <div>Developer:</div> <a href="https://store.steampowered.com/developer/valve">Valve</a>
+            <div className="devRow">
+                <div className="devRowText">Developer:</div> <a className="devRowLink" href="https://store.steampowered.com/developer/valve">Valve</a>
                 
             </div>
-            <div>
-                <div>Publisher:</div><a href="https://store.steampowered.com/developer/valve">Valve</a>
+            <div className="devRow">
+                <div className="devRowText">Publisher:</div><a className="devRowLink"href="https://store.steampowered.com/developer/valve">Valve</a>
             </div>
-            <div>
-                <p>Popular user-defined tags for this product:</p>
+            <div className="tagIntro">
+                Popular user-defined tags for this product:
+            </div>
+            <div className="tagDiv">
                 <UserDefinedTags />
             </div>
         </div>
