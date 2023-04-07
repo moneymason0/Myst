48af4fd7df0fb0ce42133e7dec8028344a13325e
Bill C Tomasello <billytomasello1@gmail.com>
2023-04-07 12:01:35 -0400

    header component hello world text added to see on screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      < Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ client/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+function Header(){
+    return 
+    <div>
+Hello world
+    </div>
+}
+export default Header
