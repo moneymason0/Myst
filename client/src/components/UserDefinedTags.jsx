eec4034940f5c68a65decc585ab384175ba46c57
alphie1028 <alphie1028@gmail.com>
2023-04-07 15:42:51 -0400

    Layed out all of the component hard coded, no functionaility, made aditional child component for tags

---
+++ client/src/components/UserDefinedTags.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from "react";
+
+function UserDefinedTags(){
+    return(
+        <div>
+            <button>
+                FPS
+            </button>
+            <button>
+                Sci-Fi
+            </button>
+            <button>
+                1990's
+            </button>
+            <button>
+                Multiplayer
+            </button>
+            <button>
+                Classic
+            </button>
+            <button>
+                Gore
+            </button>
+            <button>
+                +
+            </button>
+        </div>
+    )
+}
+export default UserDefinedTags;
