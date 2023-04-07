0a0f9613ab39a1172d4d8412000d3c0692ca88a0
alphie1028 <alphie1028@gmail.com>
2023-04-07 16:30:24 -0400

    changed format for GameDescription component to be more in line with the refrence material

--- client/src/components/GameDescription.jsx
+++ client/src/components/GameDescription.jsx
@@ -5,25 +5,31 @@ import UserDefinedTags from "./UserDefinedTags";
 function GameDescription(){
     return(
         <div>
-            <img src={pic}/>
             <div>
-                <p>Named Game of the Year by over 50 publications, 
-                    Valve's debut title blends action and adventure with award-winning technology to create a frighteningly realistic world where players must think to survive. 
-                    Also includes an exciting multiplayer mode that allows you to play against friends and enemies
-                </p>
+                <img src={pic}/>
+            </div>
+            
+            <div>
+                Named Game of the Year by over 50 publications, 
+                Valve's debut title blends action and adventure with award-winning technology to create a frighteningly realistic world where players must think to survive. 
+                Also includes an exciting multiplayer mode that allows you to play against friends and enemies
+                
             </div>
             <div>
-                <button>Recent Reviews: Overwelmingly Possive 1,666</button>
+                Recent Reviews: <span>Overwelmingly Possive 1,666</span>
             </div>
             <div>
-                <button>All Reviews: Overwelmingly Positive 70,356</button>
+                All Reviews: <span>Overwelmingly Positive 70,356</span>
             </div>
             <div>
-                <p>Release Date: Nov 8, 1998</p>
+                <div>Release Date:</div> <div>Nov 8, 1998</div>
             </div>
             <div>
-                <p>Developer: <button>Valve</button></p>
-                <p>Publisher: <button>Valve</button></p>
+                <div>Developer:</div> <a href="https://store.steampowered.com/developer/valve">Valve</a>
+                
+            </div>
+            <div>
+                <div>Publisher:</div><a href="https://store.steampowered.com/developer/valve">Valve</a>
             </div>
             <div>
                 <p>Popular user-defined tags for this product:</p>
