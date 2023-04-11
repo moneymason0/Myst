69b582ff5f38210a1f4876da4ae67822b2fb379c
alphie1028 <alphie1028@gmail.com>
2023-04-11 11:03:48 -0400

    layed out divs for share modal

--- client/src/components/ShareModal.jsx
+++ client/src/components/ShareModal.jsx
@@ -2,6 +2,43 @@ import React from "react";
 import "/src/styles/ShareModal.css"
 
 function ShareModal(){
-
+    <div className="smContainer">
+        <div className="smTopBar"></div>
+        <div className="smHeaderBoarder">
+            <div className="smHeader">
+                <div className="smX"></div>
+                <div className="smText">Share</div>
+            </div>
+        </div>
+        <div className="smContentBoarder">
+            <div className="smContentContainer">
+                <div>
+                    <div className="smContent">
+                        <div className="smDialog">
+                            <a className="smIconTag" href="" title="">
+                                <img className="smIcon" src=""></img>
+                            </a>
+                            <a className="smIconTag" href="" title="">
+                                <img className="smIcon" src=""></img>
+                            </a>
+                            <a className="smIconTag" href="" title="">
+                                <img className="smIcon" src=""></img>
+                            </a>
+                            <div className="smLink">
+                                <label></label>
+                                <textArea className="smTextBox"></textArea>
+                            </div>
+                            <div className="smResult"></div>
+                        </div>
+                        <div className="smFooter">
+                            <div className="smBtn">
+                                <span className="smBtnLabel">OK</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 export default ShareModal;
