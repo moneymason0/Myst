b314bf0b4f5fb64a03fa5c661b1dc50ca1995c80
Bill C Tomasello <billytomasello1@gmail.com>
2023-04-11 16:45:18 -0400

    push of origin nav-bar

---
+++ client/src/components/Nav-bar.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import "../styles/Nav-bar.css";
+
+const yourStore= ['Home','Community Reccomendations','Recently Viewed','Steam Curators']
+const newAndNoteWorthy= ['Steam replay 2022', 'Top Sellers','Most Played','New & Trending', 'Special Offers', 'Recently Updated','Popular Incoming']
+const categories= {'SPECIAL SECTIONS':['Free to play','Demos','Early Access','Controller Friendly','Remote Play', ]}
