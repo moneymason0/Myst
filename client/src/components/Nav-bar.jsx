105bbb150110ba87a69930912e2be594a8bcb21e
unknown <billytomasello1@gmail.com>
2023-04-11 17:50:17 -0400

    created arrays sub-categorizing the genre drop down

--- client/src/components/Nav-bar.jsx
+++ client/src/components/Nav-bar.jsx
@@ -13,4 +13,47 @@ const categories= {'SPECIAL SECTIONS':['Free to play','Demos','Early Access','Co
 'Sports & Racing':['All Sports', 'Fishing & Hunting','Individual Sports','Racing','Racing Sim','Sports Sim','Team Sports',]}
 const themes= ['Anime','Horror','Mystery & Detective','Open World','Sci-Fi & Cyberpunk','Space','Survival']
 const playerSupport = ['Co-Operative','LAN','Local & Party','MMO','Multiplayer','Online Competitive','Singleplayer']
+// these are sub categories- go the Genre tab divided by genre so they can be separated as their own elements and distributed appropriately
+const specialSections = categories['SPECIAL SECTIONS'];
+const genres = categories['GENRES'];
+const action = genres[0]['action'];
+const adventure = categories['Adventure'];
+const rolePlaying = categories['Role-Playing'];
+const simulation = categories['Simulation'];
+const strategy = categories['Strategy'];
+const sportsRacing = categories['Sports & Racing'];
 
+
+
+// parameters are the dropDownList inserted into the component, and the fn used
+// to handle the click on the drop down
+const DropDowns = ({ dropDownList, handleDropdownClick }) => {
+    // selected option is the variable set blank here,
+    // setSelectedOption is the fn that determines its value
+    const [selectedOption, setSelectedOption] = useState("");
+  
+
+    // the eventListener this fn will be attached to 
+    // will trigger the setSelectedOption fn
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+      handleDropdownClick(event);
+    };
+  
+  
+  
+  
+    const renderDropDownList = dropDownList.map((listItem, index) => (
+      <option key={index}>{listItem}</option>
+    ));
+  
+    return (
+      <>
+        <select onClick={handleDropdownClick} value={selectedOption} onChange={handleChange}>
+          {renderDropDownList}
+        </select>
+      </>
+    );
+  };
+  
+  export default DropDowns
