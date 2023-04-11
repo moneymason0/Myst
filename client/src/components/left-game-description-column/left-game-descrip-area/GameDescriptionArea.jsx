3096ffbad685c20484ab082855aaa34e7883e291
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-04-11 16:59:01 -0500

    Added many components for the left hand description portion

---
+++ client/src/components/left-game-description-column/left-game-descrip-area/GameDescriptionArea.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import BuyHalfLife from './BuyHalfLife';
+import ContentForGame from './ContentForGame';
+import PurchaseBundle1 from './PurchaseBundle1';
+import PurchaseBundle2 from './PurchaseBundle2';
+import PurchaseBundle3 from './PurchaseBundle3';
+
+function GameDescriptionArea() {
+    return (
+        <div>Game Description Area</div>
+    )
+}
+
+export default GameDescriptionArea;
