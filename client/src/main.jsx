aed9feb824d85a188b4ffbed63fa2e6caf5c1166
alphie1028 <alphie1028@gmail.com>
2023-04-12 15:50:50 -0400

    completed the language component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,17 @@ import App from './App'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { ModalProvider } from './context/ModalContext'
+import { GameLanguagesContextProvider } from './context/GameLanguagesContext'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <GameLanguagesContextProvider>
+      <ModalProvider>
+        <App />
+      </ModalProvider> 
+    </GameLanguagesContextProvider>
+
   </React.StrictMode>,
 )
