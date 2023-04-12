55fd6e02c716a8b2d48ffa979f90cb071c53f0b8
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-04-12 17:46:21 -0500

    Updated buy half life component to be more 1:1

--- client/src/components/left-game-description-column/left-game-descrip-area/GameDescriptionArea.jsx
+++ client/src/components/left-game-description-column/left-game-descrip-area/GameDescriptionArea.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-// import BuyHalfLife from './BuyHalfLife';
+import BuyHalfLife from './BuyHalfLife';
 // import ContentForGame from './ContentForGame';
 // import PurchaseBundle1 from './PurchaseBundle1';
 // import PurchaseBundle2 from './PurchaseBundle2';
@@ -7,15 +7,11 @@ import React, { useContext } from 'react';
 
 function GameDescriptionArea() {
     return (
-        <div className='game_area_purchase_game' id='game_area_purchase_section_add_to_cart_34'>
-            <form name='add_to_cart_34' action='https://store.steampowered.com/cart/' method='POST'>
-                <input type='hidden' name='' value='' />
-                <input type='hidden' name='' value='' />
-                <input type='hidden' name='' value='' />
-                <input type='hidden' name='' value='' />
-                <input type='hidden' name='' value='' />
-            </form>
-        </div>
+            <BuyHalfLife />
+            // <PurchaseBundle1 />
+            // <PurchaseBundle2 />
+            // <PurchaseBundle3 />
+            // <ContentForGame />
     )
 }
 
