eadd94cdf664493bc4d82c4dfbb3c634486db615
andmoo71 <andrewmooree@gmail.com>
2023-04-13 15:56:27 -0700

    hot fix for game description picture

--- client/src/components/left-game-description-column/WhatCuratorsSay.jsx
+++ client/src/components/left-game-description-column/WhatCuratorsSay.jsx
@@ -1,24 +1,24 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import '/src/styles/WhatCuratorsSay.css'
 
 function Curator() {
-    return (
-      <div className="curator-box">
+  return (
+    <div className="curator-box">
+      <div>
         <div>
-          <div>
-            <a className="game_description_column-view-all" href="">
-              View All
-            </a>
-          </div>
-          <h2 className="what-curators-say" >What curators say</h2>
-          <div className="curator-review" >
-            1,599 Curators have reviewed this product. Click
-            <a href=""> here </a>
-            to see them
-          </div>
+          <a className="game_description_column-view-all" href="">
+            View All
+          </a>
+        </div>
+        <h2 className="what-curators-say">What curators say</h2>
+        <div className="curator-review">
+          1,599 Curators have reviewed this product. Click
+          <a href=""> here </a>
+          to see them
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Curator;
