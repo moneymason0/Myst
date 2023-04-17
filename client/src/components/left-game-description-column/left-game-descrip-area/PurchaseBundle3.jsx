3911352115c633b93cd62a9d035dd1f1ba8577b5
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-04-17 16:01:04 -0500

    Updated bundle components almost 1:1 now

---
+++ client/src/components/left-game-description-column/left-game-descrip-area/PurchaseBundle3.jsx
@@ -0,0 +1,84 @@
+import React, { useContext } from 'react';
+import BundleContext from "../../../context/BundleContext";
+import '../styles/PurchaseBundle.css'
+
+function PurchaseBundle3() {
+    const { showGame, handleMouseEnter, handleMouseExit } = useContext(BundleContext);
+
+    return (
+        <div className="game_bundle_wrapper game_bundle">
+
+            <form name="add_bundle_to_cart_232">
+            </form>
+            <div className="game_bundle_area_purchase_platform">
+                <span className='bundle1_platform_img win'></span>
+                <span className='bundle1_platform_img mac'></span>
+            </div>
+            <h1 className='bundle1_h1'>Buy Half-Life Anthology
+                <span className='bundle_label' tooltip-text="Bundles are a special discount on a set of products.  If you already own some of the products contained in the bundle, purchasing the bundle will allow you to 'complete the set', paying only for the products you don't already own while still receiving the full bundle discount on each of those products.">BUNDLE</span>
+                <span className='bundle_label_tooltip' >(?)</span>
+            </h1>
+            <p className='package_contents'>
+                <div className='bundle_deal'>Buy this bundle to save 45% off all 4 items!</div>
+                <div className='bundle_contents_preview collapsed'>
+                    <div className='bundle_contents_preview_position'>
+                        <a>
+                            <img className='first_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='second_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='third_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='fourth_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='fifth_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='sixth_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='seventh_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='eighth_game_inside_bundle3' />
+                        </a>
+                        <a>
+                            <img className='ninth_game_inside_bundle3' />
+                        </a>
+                    </div>
+                </div>
+            </p>
+            <div className='game_purchase_action'>
+                <div className="bundle_purchase_action_bg">
+                    <div className='btn_addtocart btn_package info'>
+                        <a className='btn_blue_steamui btn_package info'>
+                            <span>Bundle Info</span>
+                        </a>
+                    </div>
+                </div>
+                <div className='bundle_purchase_action_bg'>
+                    <div className='discount_block game_purchase_discount no_discount'>
+                        <div className='bundle_base_discount'>-55%</div>
+                        <div className='discount_prices'>
+                            <div className='discount_final_price your_price'>
+                                <div className='your_price_label'>Your Price:</div>
+                                <div>$65.70</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='btn_addtocart'>
+                        <a className='btn_green_steamui btn_medium'>
+                            <span>Add to Cart</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PurchaseBundle3;
