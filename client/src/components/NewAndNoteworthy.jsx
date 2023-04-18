76434c1b2a34e1923e95eb3bc90adffaed9b42ba
Bill C Tomasello <billytomasello1@gmail.com>
2023-04-18 12:15:03 -0400

    deleting contexts from smaller divs for categories

--- client/src/components/NewAndNoteworthy.jsx
+++ client/src/components/NewAndNoteworthy.jsx
@@ -6,8 +6,9 @@ import ModalContext from "../context/ModalContext";
 function NewAndNoteWorthy() {
     const {handleNewAndNoteWorthyMouseExit} = useContext(ModalContext) ;
     const {handleNewAndNoteWorthyMouseOver} = useContext(ModalContext) ;    
+    
     return(
-        <div className="popUpContainerNoteWorthy" onMouseLeave={handleNewAndNoteWorthyMouseExit}>
+        <div className="popUpContainerNoteWorthy" onMouseEnter={handleNewAndNoteWorthyMouseOver} onMouseLeave={handleNewAndNoteWorthyMouseExit}>
             <a className="item" href="https://store.steampowered.com/login/?redir=replay%3Fsrc%3D7%26snr%3D1_5_9__12&redir_ssl=1&snr=1_5_9__12">Steam Replay 2022</a>
             <a className="item" href="https://store.steampowered.com/charts/topselling/?snr=1_5_9__12">Top Sellers</a>
             <a className="item" href="https://store.steampowered.com/charts/mostplayed/?snr=1_5_9__12">Most Played</a>
@@ -17,4 +18,5 @@ function NewAndNoteWorthy() {
             <a className="item" href="https://store.steampowered.com/explore/upcoming/?snr=1_5_9__12">Popular Incoming</a>
         </div>)
 }
+
 export default NewAndNoteWorthy
